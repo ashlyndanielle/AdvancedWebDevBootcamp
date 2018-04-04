c82b90796386b6528a52938bffb301ec08fe09e9
Ashlyn Mitros <ashlyndmitros@gmail.com>
2018-04-04 15:47:44 -0600

    finished XHR section

---
+++ Ajax/XHRExamples.js
@@ -0,0 +1,21 @@
+var XHR = new XMLHttpRequest();
+// onreadystatechange calls the function everytime
+// the state changes
+    // 0 UNSENT: client has been created; open() not called yet
+    // 1 OPENED: open() has been called
+    // 2 HEADERS_RECEIVED: send() has been called; headers and status available
+    // 3 LOADING: downloading; responseText holds partial data
+    // 4 DONE: operation complete
+XHR.onreadystatechange = function() {
+  // if the readyState is DONE and the request came through...
+  if (XHR.readyState == 4 && XHR.status == 200) {
+    console.log(XHR.responseText);
+  } else if (XHR.status != 200) {
+    // do something else...
+    console.log('There was a problem!');
+  }
+};
+
+// this states what type of request and what we are requesting
+XHR.open("GET", "https://api.github.com/zen123321");
+XHR.send();
--- Ajax/notes.md
+++ Ajax/notes.md
@@ -1,3 +1,4 @@
+
 *** AJAX ***
   A_asynchronous
   J_javaScript
@@ -10,3 +11,20 @@
   * With AJAX websites can send and request data from a server *in the background*
   * without disturbing the current page
   * this lead to single page apps and frameworks
+
+*** XML and JSON ***
+  formats used to send data through API's
+
+  *XML* extended markup language
+    sytactically similar to HTML
+  *JSON* JavaScript Object Notation
+    looks (almost) exactly like a javaScript object
+
+*** MAKING REQUESTS WITH JAVASCRIPT ***
+
+  1. XMLHTTP Request ( XHR )
+  2. Fetch API
+  3. 3rd Party Libraries:
+      - jQuery
+      - Axios
+
