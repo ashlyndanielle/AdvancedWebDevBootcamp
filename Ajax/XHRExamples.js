bdf8a98cd1a9c5329c8f49978d244a520deee901
Ashlyn Mitros <ashlyndmitros@gmail.com>
2018-04-05 12:23:08 -0600

    tweaked bitcoin checker

--- Ajax/XHRExamples.js
+++ Ajax/XHRExamples.js
@@ -19,3 +19,20 @@ XHR.onreadystatechange = function() {
 // this states what type of request and what we are requesting
 XHR.open("GET", "https://api.github.com/zen123321");
 XHR.send();
+
+
+// value of checked input button
+// form being the form container
+// name being the radio button name group
+function getRadioValue(form, name) {
+  let val;
+  const radios = form.elements[name];
+  for (let x = 0; x < radios.length; x++) {
+    if ( radios[x].checked ) {
+      val = radios[x].value;
+      break;
+    }
+  }
+  console.log('VALUE: ', val);
+  return val;
+} 
