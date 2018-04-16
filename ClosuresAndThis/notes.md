45bef71bd8f805f8af6d762d15e4d90c9477b516
Ashlyn Mitros <ashlyndmitros@gmail.com>
2018-04-16 14:10:39 -0600

    completed section on *this* keyword

--- ClosuresAndThis/notes.md
+++ ClosuresAndThis/notes.md
@@ -23,17 +23,37 @@
       3. explicit
       4. new
 
-  1. GLOBAL CONTEXT
+  1. *GLOBAL CONTEXT*
     - when *this* is not inside of a declared object
     - there hasn't been an object literal defined that contains
       a function that uses keyword *this*
     - value refers to the global object ( window in the browser )
       console.log(this) // window
 
-  2. OBJECT/IMPLICIT
+  2. *OBJECT/IMPLICIT*
     - the value of *this* will always be the *closest* parent
       object when used inside of a declared object
     - *this* is defined when a function is run ( this is why you use
       the constructor functions! )
-  3. EXPLICIT: CALL/APPLY/BIND
-    - 
+  3. *EXPLICIT: CALL/APPLY/BIND*
+    - call, apply and bind are only methods that can be used by functions,
+      not other data types
+    - *CALL*:
+        first parameter: thisArg ( whatever you want the value of *this* to be )
+        other parameters: any parameters you want to pass to the function
+          that you are calling .call() on
+        when .call() is used, that function is immediately invoked
+    - *APPLY*:
+        only takes in two parameters at most
+        first parameter: thisArg
+        second parameter: array of parameters to pass into the function
+        immediately invoked
+        this will spead out the array you pass in as the arguments - useful
+        when the function doesn't take an array as a parameter (Math.max())
+    - *BIND*:
+        almost the same as .call()
+        returns a function definition, does not invoke immediately
+        can use this definition elsewhere with the context that you chose
+  4. *NEW KEYWORD*
+    - the value of *this* is set to be an empty object and returned
+      from the function that is invoked with the new keyword
--- ClosuresAndThis/this.js
+++ ClosuresAndThis/this.js
@@ -32,7 +32,7 @@ console.log(person.determineContext());
 
 
 
-var anotherPerson = {
+const anotherPerson = {
   firstName: 'Ashlyn',
   // *this* will still refer to the global object not anotherPerson
   // because it's defined when a function is run
@@ -40,3 +40,139 @@ var anotherPerson = {
 }
 
 console.log(anotherPerson.determineContext)
+
+// explicit
+const stopHiccuping = {
+  firstName: 'Stupid',
+  sayHi: function() {
+    return `Hi ${firstName}`;
+  },
+  determineContext: function() {
+    return this === person;
+  },
+  dog: {
+    sayHello: function() {
+      return `Bark bark ${this.firstName}`;
+    },
+    determineContext: function() {
+      return this === person;
+    }
+  }
+}
+
+console.log(stopHiccuping.dog.sayHello.call(stopHiccuping));
+console.log(stopHiccuping.dog.sayHello.apply(stopHiccuping));
+
+sayHello = stopHiccuping.dog.sayHello.bind(stopHiccuping);
+console.log(sayHello());
+
+
+// reusing code:
+const colt = {
+  firstName: 'Colt',
+  sayHi: function() {
+    return `Hi ${this.firstName}`;
+  }
+}
+const elie = {
+  firstName: 'Elie'
+}
+
+console.log(colt.sayHi());
+console.log(colt.sayHi.call(elie));
+
+// even better...
+
+function sayWhatUp() {
+  return `What up ${this.firstName}!`
+}
+
+const gus = {
+  firstName: 'Gus'
+}
+
+const ash = {
+  firstName: 'Ashlyn'
+}
+
+console.log(sayWhatUp.call(gus));
+console.log(sayWhatUp.call(ash));
+// ------- or ------- //
+gus.sayWhatUp = sayWhatUp;
+ash.sayWhatUp = sayWhatUp;
+console.log(gus.sayWhatUp())
+console.log(ash.sayWhatUp())
+
+
+// -- example use cases -- //
+
+// select all the divs on a page and find
+// the ones that have the text "Hello"
+
+// can't use .filter because the list of div's
+// is not an array but an array like object
+// have to convert it into an array using .slice
+
+// instead of the target of slice being that array,
+// set the target of *this* to be our div's object
+
+// const divs = document.querySelectorAll('div');
+// var divsArray = [].slice.call(divs);
+// -- or --
+// var divsArray = Array.prototype.slice.call(divs);
+
+// divsArray.filter( val => {
+//   return val.innerText === "Hello";
+// })
+
+function arrayFrom(arrayLikeObject) {
+  return [].slice.call(arrayLikeObject);
+}
+
+
+
+// ***** CALL VS APPLY ***** //
+
+function addNumbers(a,b,c,d) {
+  return `${this.firstName} just calculated ${a+b+c+d}`;
+}
+
+console.log(addNumbers.call(gus, 1, 2, 3, 4));
+console.log(addNumbers.apply(ash, [1, 2, 3, 4]));
+
+
+// apply will spread out the array for you
+const nums = [2, 3, 4];
+// Math.max will not accept an array as an argument
+console.log(Math.max(nums));
+console.log(Math.max.apply(this, nums))
+
+function sumValues(a,b,c) {
+  return a+b+c;
+}
+
+console.log(sumValues(nums));
+console.log(sumValues.apply(this, nums));
+
+
+// setTimeout and .bind
+
+const blah = {
+  food: 'Apples',
+  eatFood: function() {
+    setTimeout( () => {
+      console.log(`I'm going to eat some ${this.food} and ${this} is my context`);
+    }, 1000)
+  }
+}
+
+const eatFood = blah.eatFood.bind(blah);
+console.log(eatFood())
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+const mom = new Person( "Betsy", "Erickson" );
+console.log(mom);
