a113710dab4ee74a033cd77854d16aae3d75e5b8
Ashlyn <ashlyn@Admins-MacBook-Pro.local>
2019-05-07 11:50:51 -0600

    completed exercise with instructor

--- D3/DataJoinsAndUpdatePatterns/FrequencyCharacters/app.js
+++ D3/DataJoinsAndUpdatePatterns/FrequencyCharacters/app.js
@@ -2,71 +2,64 @@ const input = d3.select('input');
 const countButton = d3.select('button[type="submit"');
 const reset = d3.select('#reset');
 
-let previousChars = [];
-let currentChars = [];
-let combinedChars = [];
-
 countButton.on('click', updateCharacterData);
-
-reset.on('click', () => {
-	d3.event.preventDefault();
-	previousChars = [];
-	currentChars = [];
-	d3.selectAll('.letter')
-})
-
-
-
-
-
-
+reset.on('click', resetAnalysis)
 
 function updateCharacterData() {
 	d3.event.preventDefault();
 	let inputValue = input.property('value');
 	if (inputValue) {
-		// count characters
-		let newChars = inputValue.match(/[a-zA-Z]/g).reduce((acc, next) => {
-			acc[next] ? acc[next]++ : acc[next] = 1;
-			return acc;
-		}, {});
-		let charObject = [];
-		// create usable data
-		for (let key in newChars) {
-			charObject.push({'letter': key, 'count': newChars[key]})
-		}
-		console.table(charObject)
-		// update data catagories
-		if (currentChars) {
-			previousChars = currentChars;
-		}
-		currentChars = charObject;
-	}
-
-	input.property('value', '');
-
-	console.log('PREV__________________');
-	console.table(previousChars);
-	console.log('CURR__________________')
-	console.table(currentChars);
-
-	// update
-	// d3.select('#letters')
-	// 	.selectAll('div')
-	// 	.data(currentChars)
-	// exit
-	d3.selectAll('.letter')
-		.data(currentChars, d => d.letter)
-		.exit()
-		.remove()
-	// enter
-	d3.select('#letters')
-		.selectAll('div')
-		.data(currentChars, d => d.letter)
-		.enter()
-		.append('div')
+		// update
+		let letters = d3.select('#letters')
+			.selectAll('div')
+			.data(countFrequencies(inputValue), d => d.letter)
+		letters
+			.classed('new', false)
+			// exit
+			.exit()
+			.remove()
+		// enter
+		letters
+			.enter()
+			.append('div')
+			.classed('letter', true)
+			.classed('new', true)
+			// merge
+			.merge(letters)
 			.text(d => d.letter)
-			.classed('letter new', true)
 			.style('padding-top', d => `${d.count-1}em`)
-	// merge
+
+		d3.select('#phrase')
+			.text(`Analysis of: ${inputValue}`)
+		d3.select('#count')
+			.text(`(New Characters: ${letters.enter().nodes().length})`)
+		input.property('value', '');
+	}
+}
+
+function resetAnalysis() {
+	d3.selectAll('.letter')
+		.remove()
+	d3.select('#phrase')
+			.text('');
+	d3.select('#count')
+			.text('');
+}
+
+function countFrequencies(input) {
+	// count characters
+	let charCount = input.match(/[a-zA-Z]/g).sort().reduce((acc, next) => {
+		acc[next] ? acc[next]++ : acc[next] = 1;
+		return acc;
+	}, {});
+
+	// create usable data
+	let data = [];
+	for (let key in charCount) {
+		data.push({
+			'letter': key,
+			'count': charCount[key]
+		})
+	}
+	return data;
 }
